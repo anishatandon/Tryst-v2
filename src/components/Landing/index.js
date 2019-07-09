d84386f069decd702ddd04363291101823fe9bd3
anishatandon <atandon@g.hmc.edu>
2019-07-08 17:53:40 -0700

    began beautifying process

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,22 +1,37 @@
 import React from 'react';
 import logo from '../../logo.svg';
 import { Link } from 'react-router-dom';
-import './App.css';
-
+import { Navbar } from 'react-bootstrap';
+// import './App.css';
+import '../../alternatecss.css';
 
 const Landing = () => (
-  <div className="App">
-      <header className="header">
-        <img src={logo} className="logo" alt="logo" />
-        <h2 className = {"great-vibes"}>
-          Tryst
-          </h2>
-          <body className="garamond-h3">
-            Dating without the hassle <br></br><br></br>
-          <Link to='/SignUp' className="App-link">Sign Up</Link><br></br><br></br>
-          <Link to='/Signin' className="App-link">Log in</Link>
-          </body>
-      </header></div>
+    <div className="navbar" >
+      {/* <section className="st-container"> */}
+      {/* <a href = "#home" class = "active"> Home </a> */}
+        <Link to='/Signin' className="link">Sign in</Link>
+    </div>
+      //     {/* <input type="radio" name="radio-set" id="st-control-2"/> */}
+
+
+      //     {/* <input type="radio" name="radio-set" id="st-control-2"/> */}
+      //     <Link to='/Signup' className="App-link">Sign Up</Link>
+
+
+      //     <img src={logo} className="logo" alt="logo" />
+      //     <h1> Tryst </h1>
+      //     <h2>Dating without the hassle </h2>
+
+      // {/* </section>  */}
+
+      // <div className = "text">
+      //   <h2 className = {"great-vibes"}>
+      //     Tryst
+      //     </h2>
+      //       Dating without the hassle <br></br><br></br>
+      //     <Link to='/SignUp' className="App-link">Sign Up</Link><br></br><br></br>
+      //     <Link to='/Signin' className="App-link">Log in</Link>
+      // </div>
 );
 
 export default Landing;
