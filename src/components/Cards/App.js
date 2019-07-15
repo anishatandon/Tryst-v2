2d41454976ac9b92bb204ee7b62b913eeec9844b
anishatandon <atandon@g.hmc.edu>
2019-07-15 16:30:51 -0700

    now we have a name for swipes that work

--- src/components/Cards/App.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import Deck from "../Cards/Deck";
-
-const App = () => <Deck />;
-
-export default App;
--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -1,52 +1,87 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { animated, interpolate } from "react-spring/hooks";
-import Carousel from "nuka-carousel";
+import React, { Component } from "react";
+import { render } from "react-dom";
 
-class Card extends React.Component {
+import Swipeable from "react-swipy";
+import Button from "../Cards/Button";
+import Card1 from "./Card1";
+import Card2 from "./Card2";
+import Data from "../Cards/data";
+
+const cardStyles = {
+  background: "whitesmoke",
+  borderRadius: 3,
+  width: "250px",
+  height: "250px",
+  cursor: "pointer",
+  userSelect: "none",
+  position: "absolute",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  top: 0
+};
+
+const Card = ({ zIndex = 0, children }) => (
+  <div style={{ ...cardStyles, zIndex }}>{children}</div>
+);
+
+
+const appStyles = {
+  height: "100%",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  width: "100%",
+  minHeight: "100vh",
+  fontFamily: "sans-serif",
+  overflow: "hidden"
+};
+
+const wrapperStyles = { position: "relative", width: "250px", height: "250px" };
+const actionsStyles = {
+  display: "flex",
+  justifyContent: "space-between",
+  marginTop: 12
+};
+
+
+class Cards extends Component {
+  state = {
+    cards: Data
+
+  };
+
+  remove = () =>
+    this.setState(({ cards }) => ({ cards: cards.slice(1, cards.length) }));
+
   render() {
-    const { i, x, y, rot, scale, trans, bind, data } = this.props;
-    const { name, age, distance, text, pics } = data[i];
+    const { cards } = this.state;
 
     return (
-      <animated.div
-        key={i}
-        style={{
-          transform: interpolate(
-            [x, y],
-            (x, y) => `translate3d(${x}px,${y}px,0)`
-          )
-        }}
-      >
-        <animated.div
-          {...bind(i)}
-          style={{
-            transform: interpolate([rot, scale], trans)
-          }}
-        >
-          <div className="card">
-            <Carousel>
-              {pics.map((pic, index) => (
-                <img src={pic} key={index} alt="profilePicture" />
-              ))}
-            </Carousel>
-            <h2>{name},</h2>
-            <h2>{age}</h2>
-            <h5>{distance}</h5>
-            <h5>{text}</h5>
-          </div>
-        </animated.div>
-      </animated.div>
+      <div style={appStyles}>
+        <div style={wrapperStyles}>
+          {cards.length > 0 && (
+            <div style={wrapperStyles}>
+              <Swipeable
+                buttons={({ right, left }) => (
+                  <div style={actionsStyles}>
+                    <Button onClick={left}>Reject</Button>
+                    <Button onClick={right}>Accept</Button>
+                  </div>
+                )}
+                onAfterSwipe={this.remove}
+              >
+                <Card>{cards[0]}</Card>
+              </Swipeable>
+              {cards.length > 1 && <Card zIndex={-1}>{cards[1]}</Card>}
+            </div>
+          )}
+          {cards.length <= 1 && <Card zIndex={-2}>No more cards</Card>}
+        </div>
+      </div>
     );
   }
 }
 
-Card.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number,
-  distance: PropTypes.string,
-  text: PropTypes.string,
-  pics: PropTypes.array
-};
 
-export default Card;
+export default Cards;
